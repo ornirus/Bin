cbb1c2421ec4f380994028fd4af31373ecca0f8b
ornirus <alen.alijevic@gmail.com>
2014-12-04 14:22:59 +0100

    tests

--- src/co/slzr/Main.java
+++ src/co/slzr/Main.java
@@ -3,14 +3,7 @@ package co.slzr;
 public class Main {
 
     public static void main(String[] args) {
-        Binary bin = new Binary("10111011");
-
-        System.out.println(bin.toString());
-        System.out.println(bin.toInt());
-
-        Binary bin2 = new Binary(20);
-
-        System.out.println(bin2.toString());
-        System.out.println(bin2.toInt());
+        Binary binary = new Binary("0011010010100101");
+        System.out.println(binary.toDecimal());
     }
 }
